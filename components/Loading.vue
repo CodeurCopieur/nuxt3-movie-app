5a33667b9a6a2c7af6ee2dc33e79ce5322dc1ab1
Widdy LOUIS <louisws@outlook.fr>
2023-06-07 17:49:52 +0200

    Update Loading.vue

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="loading">
-    <span></span>
-  </div>
+
+    <div class="flex justify-center items-center">
+      <span class="my-5 border-b-4 border-blue-800 current text-center outline-none text-white">
+          <div class="loading">
+            <span></span>
+          </div>
+        </span>  
+    </div>
+
 </template>
