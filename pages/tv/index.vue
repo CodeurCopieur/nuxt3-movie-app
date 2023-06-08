79ce70803978faa2e1a8630bfbd7c6809482cff6
Widdy LOUIS <louisws@outlook.fr>
2023-06-09 01:22:10 +0200

    Update index.vue

--- pages/tv/index.vue
+++ pages/tv/index.vue
@@ -1,3 +1,12 @@
+<script setup>
+  useHead({
+    title: 'Tv Shows | Nuxt 3 and Themoviedb',
+    meta: [
+      {name: 'description', content: 'Cette page les series & émissions de télévision'}
+    ]
+  });
+</script>
+
 <template>
  <PageHeader title="TV"></PageHeader>
  <section>
