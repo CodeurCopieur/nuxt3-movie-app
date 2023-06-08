e265f36209543f128698c6e6f966f931bb9b0708
Widdy LOUIS <louisws@outlook.fr>
2023-06-09 01:22:06 +0200

    Update index.vue

--- pages/movie/index.vue
+++ pages/movie/index.vue
@@ -1,3 +1,12 @@
+<script setup>
+  useHead({
+    title: 'Movie | Nuxt 3 and Themoviedb',
+    meta: [
+      {name: 'description', content: 'Cette page les films'}
+    ]
+  });
+</script>
+
 <template>
  <PageHeader title="Movie"></PageHeader>
  <section>
