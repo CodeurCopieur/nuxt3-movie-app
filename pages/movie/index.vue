a1ba546d8580d0810e7f7c2d66c161a24ce2addb
Widdy LOUIS <louisws@outlook.fr>
2023-06-11 10:00:17 +0200

    add slider (movies tv)

--- pages/movie/index.vue
+++ pages/movie/index.vue
@@ -2,15 +2,22 @@
   useHead({
     title: 'Movies | Nuxt 3 and Themoviedb',
     meta: [
-      {name: 'description', content: 'Les films'}
+      {name: 'description', content: 'Les films tendance et classées par popularité'}
     ]
   });
+
+    const timeWindow = 'week'; // Remplacez 'week' par la fenêtre de temps souhaitée
+  const state = {
+    allMovies: [],
+    timeWindow: 'week'
+  }
+  state.allMovies = await useMoviesApi().getMovies(`trending/movie/${state.timeWindow}`);
 </script>
 
 <template>
- <PageHeader title="Movie"></PageHeader>
- <section>
-  <MovieOrTvCard :type="`movie`"/>
-</section>
+  <Hero :movies="state.allMovies" type="movie" />
+  <section>
+    <MovieOrTvCard :type="`movie`"/>
+  </section>
 </template>
 
