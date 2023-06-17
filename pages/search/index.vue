0320f6a914995fc3d64382d282b467bed5e54df0
Widdy LOUIS <louisws@outlook.fr>
2023-06-17 22:34:36 +0200

    Update index.vue

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,3 +1,12 @@
+<script setup>
+  useHead({
+    title: 'Search movie or serie or actor | Nuxt 3 and Themoviedb',
+    meta: [
+      {name: 'description', content: 'Recherche de film, serie, acteur'}
+    ]
+  });
+</script>
+
 <template>
   <PageHeader title="Search"></PageHeader>
   <PageSearch></PageSearch>
