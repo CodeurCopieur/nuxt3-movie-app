546e0f45f96d75935d30287c84721cdb679d5e0c
Widdy LOUIS <louisws@outlook.fr>
2023-06-22 11:13:34 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,12 +11,12 @@
 
 <template>
  <!-- <Loading v-if="isLoading " class="fixed"/> -->
- <div class="flex bg-gray-900 text-white">
+ <div class="flex flex-col bg-gray-900 text-white">
 
     <Header/>
     <main class="w-full min-h-screen">
         <slot />
     </main>
-    <!-- <Footer/> -->
+    <Footer/>
  </div>
 </template>
