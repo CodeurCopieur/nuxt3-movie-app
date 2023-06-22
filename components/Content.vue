e18ae58f67eb5bc354295755f96edcdcf7d8071a
Widdy LOUIS <louisws@outlook.fr>
2023-06-22 11:12:49 +0200

    update style

--- components/Content.vue
+++ components/Content.vue
@@ -6,24 +6,24 @@
 </script>
 
 <template>
-   <div class="home my-8 container max-w-screen-lg max-w-screen-xl mx-auto px-4 py-8 lg:max-w-7xl">
+   <div class="home my-8 container max-w-screen-lg max-w-screen-xl mx-auto px-6 py-8 lg:max-w-7xl">
     
-    <div class="recommended px-6 mb-12">
+    <div class="recommended mb-12">
       <h2 class="text-white-600 mb-1" aria-label="Films populaires"><span class="border-b-4 border-blue-800 text-lg">Films populaires</span></h2>
       <MovieOrTvSlider :type="state.movie" theme="popular"/>
     </div>
 
-    <div class="recommended px-6 mb-12">
+    <div class="recommended mb-12">
       <h2 class="text-white-600 mb-1" aria-label="Séries populaires"><span class="border-b-4 border-blue-800 text-lg">Séries populaires</span></h2>
       <MovieOrTvSlider :type="state.tv" theme="popular"/>
     </div>
 
-    <div class="recommended px-6 mb-12">
+    <div class="recommended mb-12">
       <h2 class="text-white-600 mb-1" aria-label="Films les mieux notés"><span class="border-b-4 border-blue-800 text-lg">Films les mieux notés</span></h2>
       <MovieOrTvSlider :type="state.movie" theme="top_rated"/>
     </div>
 
-    <div class="recommended px-6 mb-12">
+    <div class="recommended mb-12">
       <h2 class="text-white-600 mb-1" aria-label="Séries les mieux notés"><span class="border-b-4 border-blue-800 text-lg">Séries les mieux notés</span></h2>
       <MovieOrTvSlider :type="state.tv" theme="top_rated"/>
     </div>
