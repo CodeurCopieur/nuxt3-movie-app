b9d348b6bbcb1560a4535335dd2bf0fd4ab72c82
Widdy LOUIS <louisws@outlook.fr>
2023-06-22 11:13:15 +0200

    Update ModalContent.vue

--- components/ModalContent.vue
+++ components/ModalContent.vue
@@ -6,9 +6,9 @@
 <template>
   <div class="modal-wrapper relative w-screen h-screen  flex justify-center items-center">
     <div class="container_story relative w-full md:w-2/3 md:h-4/5">
-            <button 
+      <button 
         class="block mx-auto md:mr-0 md:ml-auto text-center text-white bg-blue-800 text-teal-900 hover:bg-blue-500 p-3"
-      @click.self="$emit('close')"></button>
+        @click.self="$emit('close')"></button>
 
       <MovieOrTvSlider 
         type="videos" 
